29c627fa5bf0a83a342cc3811df3e814fe290c14
Christine <christinemoore321@gmail.com>
2019-01-18 20:34:28 -0600

    updates

--- js/freelancer.js
+++ js/freelancer.js
@@ -15,6 +15,41 @@
     }
   });
 
+  // $('.counter-count').each(function () {
+  //   $(this).prop('Counter',0).animate({
+  //       Counter: $(this).text()
+  //   }, {
+  //       duration: 5000,
+  //       easing: 'swing',
+  //       step: function (now) {
+  //           $(this).text(Math.ceil(now));
+  //       }
+  //   });
+  // });
+
+  // $('.Count').each(function () {
+  //   var $this = $(this);
+  //   jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+  //     duration: 1000,
+  //     easing: 'swing',
+  //     step: function () {
+  //       $this.text(Math.ceil(this.Counter));
+  //     }
+  //   });
+  // });
+
+//   $('.counter-count').each(function () {
+//     $(this).prop('Counter',0).animate({
+//         Counter: $(this).text()
+//     }, {
+//         duration: 4000,
+//         easing: 'swing',
+//         step: function (now) {
+//             $(this).text(Math.ceil(now));
+//         }
+//     });
+// });
+
   $('ul.nav li.dropdown').hover(function() {
   $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
 }, function() {
@@ -154,6 +189,33 @@ var waypoints = $('.header-up').waypoint(function(direction) {
   offset: '70%'
 })
 
+// var waypoints = $('.counter-count').waypoint(function(direction) {
+//   $(this).prop('Counter',0).animate({
+//           Counter: $(this).text()
+//       }, {
+//           duration: 5000,
+//           easing: 'swing',
+//           step: function (now) {
+//               $(this).text(Math.ceil(now));
+//           }
+//       })
+// }, {
+//   offset: '50%'
+// })
+
+var waypoints = $('.counter-count').waypoint(function() {
+    $(this).prop('Counter',0).animate({
+      Counter: $(this).text()
+  }, {
+      duration: 5000,
+      easing: 'swing',
+      step: function (now) {
+          $(this).text(Math.ceil(now));
+      }
+  })
+}, {
+  offset: '50%'
+})
 
 var waypoints = $('.fade').waypoint(function(direction) {
   $('.fade').addClass('fade-effect');
@@ -184,3 +246,350 @@ var waypoints = $('.fade3').waypoint(function(direction) {
 }, {
   offset: '80%'
 })
+
+var CountUp = function(target, startVal, endVal, decimals, duration, options) {
+
+	var self = this;
+	self.version = function () { return '1.9.3'; };
+	
+	// default options
+	self.options = {
+		useEasing: true, // toggle easing
+		useGrouping: true, // 1,000,000 vs 1000000
+		separator: ',', // character to use as a separator
+		decimal: '.', // character to use as a decimal
+		easingFn: easeOutExpo, // optional custom easing function, default is Robert Penner's easeOutExpo
+		formattingFn: formatNumber, // optional custom formatting function, default is formatNumber above
+		prefix: '', // optional text before the result
+		suffix: '', // optional text after the result
+		numerals: [] // optionally pass an array of custom numerals for 0-9
+	};
+
+	// extend default options with passed options object
+	if (options && typeof options === 'object') {
+		for (var key in self.options) {
+			if (options.hasOwnProperty(key) && options[key] !== null) {
+				self.options[key] = options[key];
+			}
+		}
+	}
+
+	if (self.options.separator === '') {
+		self.options.useGrouping = false;
+	}
+	else {
+		// ensure the separator is a string (formatNumber assumes this)
+		self.options.separator = '' + self.options.separator;
+	}
+
+	// make sure requestAnimationFrame and cancelAnimationFrame are defined
+	// polyfill for browsers without native support
+	// by Opera engineer Erik Möller
+	var lastTime = 0;
+	var vendors = ['webkit', 'moz', 'ms', 'o'];
+	for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+		window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+		window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+	}
+	if (!window.requestAnimationFrame) {
+		window.requestAnimationFrame = function(callback, element) {
+			var currTime = new Date().getTime();
+			var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+			var id = window.setTimeout(function() { callback(currTime + timeToCall); }, timeToCall);
+			lastTime = currTime + timeToCall;
+			return id;
+		};
+	}
+	if (!window.cancelAnimationFrame) {
+		window.cancelAnimationFrame = function(id) {
+			clearTimeout(id);
+		};
+	}
+
+	function formatNumber(num) {
+		var neg = (num < 0),
+        	x, x1, x2, x3, i, len;
+		num = Math.abs(num).toFixed(self.decimals);
+		num += '';
+		x = num.split('.');
+		x1 = x[0];
+		x2 = x.length > 1 ? self.options.decimal + x[1] : '';
+		if (self.options.useGrouping) {
+			x3 = '';
+			for (i = 0, len = x1.length; i < len; ++i) {
+				if (i !== 0 && ((i % 3) === 0)) {
+					x3 = self.options.separator + x3;
+				}
+				x3 = x1[len - i - 1] + x3;
+			}
+			x1 = x3;
+		}
+		// optional numeral substitution
+		if (self.options.numerals.length) {
+			x1 = x1.replace(/[0-9]/g, function(w) {
+				return self.options.numerals[+w];
+			})
+			x2 = x2.replace(/[0-9]/g, function(w) {
+				return self.options.numerals[+w];
+			})
+		}
+		return (neg ? '-' : '') + self.options.prefix + x1 + x2 + self.options.suffix;
+	}
+	// Robert Penner's easeOutExpo
+	function easeOutExpo(t, b, c, d) {
+		return c * (-Math.pow(2, -10 * t / d) + 1) * 1024 / 1023 + b;
+	}
+	function ensureNumber(n) {
+		return (typeof n === 'number' && !isNaN(n));
+	}
+
+	self.initialize = function() { 
+		if (self.initialized) return true;
+		
+		self.error = '';
+		self.d = (typeof target === 'string') ? document.getElementById(target) : target;
+		if (!self.d) { 
+			self.error = '[CountUp] target is null or undefined'
+			return false;
+		}
+		self.startVal = Number(startVal);
+		self.endVal = Number(endVal);
+		// error checks
+		if (ensureNumber(self.startVal) && ensureNumber(self.endVal)) {
+			self.decimals = Math.max(0, decimals || 0);
+			self.dec = Math.pow(10, self.decimals);
+			self.duration = Number(duration) * 1000 || 2000;
+			self.countDown = (self.startVal > self.endVal);
+			self.frameVal = self.startVal;
+			self.initialized = true;
+			return true;
+		}
+		else {
+			self.error = '[CountUp] startVal ('+startVal+') or endVal ('+endVal+') is not a number';
+			return false;
+		}
+	};
+
+	// Print value to target
+	self.printValue = function(value) {
+		var result = self.options.formattingFn(value);
+
+		if (self.d.tagName === 'INPUT') {
+			this.d.value = result;
+		}
+		else if (self.d.tagName === 'text' || self.d.tagName === 'tspan') {
+			this.d.textContent = result;
+		}
+		else {
+			this.d.innerHTML = result;
+		}
+	};
+
+	self.count = function(timestamp) {
+
+		if (!self.startTime) { self.startTime = timestamp; }
+
+		self.timestamp = timestamp;
+		var progress = timestamp - self.startTime;
+		self.remaining = self.duration - progress;
+
+		// to ease or not to ease
+		if (self.options.useEasing) {
+			if (self.countDown) {
+				self.frameVal = self.startVal - self.options.easingFn(progress, 0, self.startVal - self.endVal, self.duration);
+			} else {
+				self.frameVal = self.options.easingFn(progress, self.startVal, self.endVal - self.startVal, self.duration);
+			}
+		} else {
+			if (self.countDown) {
+				self.frameVal = self.startVal - ((self.startVal - self.endVal) * (progress / self.duration));
+			} else {
+				self.frameVal = self.startVal + (self.endVal - self.startVal) * (progress / self.duration);
+			}
+		}
+
+		// don't go past endVal since progress can exceed duration in the last frame
+		if (self.countDown) {
+			self.frameVal = (self.frameVal < self.endVal) ? self.endVal : self.frameVal;
+		} else {
+			self.frameVal = (self.frameVal > self.endVal) ? self.endVal : self.frameVal;
+		}
+
+		// decimal
+		self.frameVal = Math.round(self.frameVal*self.dec)/self.dec;
+
+		// format and print value
+		self.printValue(self.frameVal);
+
+		// whether to continue
+		if (progress < self.duration) {
+			self.rAF = requestAnimationFrame(self.count);
+		} else {
+			if (self.callback) self.callback();
+		}
+	};
+	// start your animation
+	self.start = function(callback) {
+		if (!self.initialize()) return;
+		self.callback = callback;
+		self.rAF = requestAnimationFrame(self.count);
+	};
+	// toggles pause/resume animation
+	self.pauseResume = function() {
+		if (!self.paused) {
+			self.paused = true;
+			cancelAnimationFrame(self.rAF);
+		} else {
+			self.paused = false;
+			delete self.startTime;
+			self.duration = self.remaining;
+			self.startVal = self.frameVal;
+			requestAnimationFrame(self.count);
+		}
+	};
+	// reset to startVal so animation can be run again
+	self.reset = function() {
+		self.paused = false;
+		delete self.startTime;
+		self.initialized = false;
+		if (self.initialize()) {
+			cancelAnimationFrame(self.rAF);
+			self.printValue(self.startVal);
+		}
+	};
+	// pass a new endVal and start animation
+	self.update = function (newEndVal) {
+		if (!self.initialize()) return;
+		newEndVal = Number(newEndVal);
+		if (!ensureNumber(newEndVal)) {
+			self.error = '[CountUp] update() - new endVal is not a number: '+newEndVal;
+			return;
+		}
+		self.error = '';
+		if (newEndVal === self.frameVal) return;
+		cancelAnimationFrame(self.rAF);
+		self.paused = false;
+		delete self.startTime;
+		self.startVal = self.frameVal;
+		self.endVal = newEndVal;
+		self.countDown = (self.startVal > self.endVal);
+		self.rAF = requestAnimationFrame(self.count);
+	};
+
+	// format startVal on initialization
+	if (self.initialize()) self.printValue(self.startVal);
+};
+
+var optionsNormal = {
+    useEasing : true, 
+    useGrouping : true, 
+    separator : ',', 
+    decimal : '.',
+    prefix : '',
+    suffix : '' 
+  }
+
+  var optionsDollar = {
+      useEasing : true, 
+      useGrouping : true, 
+      separator : ',', 
+      decimal : '.',
+      prefix : '$ ',
+      suffix : ' MM' 
+    }
+
+  var optionsPlus = {
+      useEasing : true, 
+      useGrouping : true, 
+      separator : ',', 
+      decimal : '.',
+      prefix : '',
+      suffix : ' +' 
+    }
+
+    var options = {
+        useEasing : true, 
+        useGrouping : true, 
+        separator : ',', 
+        decimal : '.',
+        prefix : '',
+        suffix : ' +' 
+      }
+
+
+  // count up variables
+  // target, startVal, endVal, decimals, duration, options
+
+  var scrollNum = document.getElementById("scroll-num");
+  var scrollDemo = new CountUp(scrollNum, 0, 2015, 0, 2.5, options);
+
+  var firstNum = document.getElementById("first-num");
+  var firstScroll = new CountUp(firstNum, 0, 3, 0, 4.5, optionsNormal);
+
+  var secondNum = document.getElementById("second-num");
+  var secondScroll = new CountUp(secondNum, 0, 37.5, true, 4.5, optionsDollar);
+
+  var thirdNum = document.getElementById("third-num");
+  var thirdScroll = new CountUp(thirdNum, 0, 50, 0, 4.5, optionsPlus);
+
+  var hoverNum = document.getElementById("hover-num");
+  var hoverDemo = new CountUp(hoverNum, 0, 3000, 0, 2.5, options);
+  
+  jQuery('.first-counter').waypoint(
+    function(direction){
+      if(direction == "down") {
+        firstScroll.start();
+      }
+    },
+    { offset: '70%' }
+  );
+
+  jQuery('.second-counter').waypoint(
+    function(direction){
+      if(direction == "down") {
+        secondScroll.start();
+      }
+    },
+    { offset: '70%' }
+  );
+
+  jQuery('.third-counter').waypoint(
+    function(direction){
+      if(direction == "down") {
+        thirdScroll.start();
+      }
+    },
+    { offset: '70%' }
+  );
+
+  jQuery('.region-2').waypoint(
+    function(direction){
+      if(direction == "down") {
+        scrollDemo.start();
+      }
+    },
+    { offset: '70%' }
+  );
+
+//   var scrollNum = document.getElementById("scroll-num");
+// var scrollDemo = new CountUp(scrollNum, 0, 2015, 0, 2.5, options);
+// var hoverNum = document.getElementById("hover-num");
+// var hoverDemo = new CountUp(hoverNum, 0, 3000, 0, 2.5, options);
+
+// jQuery('.region-2').waypoint(
+//   function(direction){
+//     if(direction == "down") {
+//       scrollDemo.start();
+//     }
+// 	},
+// 	{ offset: 0 }
+// );
+
+// $(".region-1 .num-wrap #hover-num").hover(function(){
+//   hoverDemo.start();
+// });
+
+  
+  $(".region-1 .num-wrap #hover-num").hover(function(){
+    hoverDemo.start();
+  });
